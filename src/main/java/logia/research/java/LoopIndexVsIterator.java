39a0ce4721e9a1d93ee19ff43ac8dc667c10afae
Dai Mai <dai.mai@mediastep.com>
2022-11-24 00:21:29 +0700

    change sample

--- src/main/java/logia/research/java/LoopIndexVsIterator.java
+++ src/main/java/logia/research/java/LoopIndexVsIterator.java
@@ -3,7 +3,6 @@ package logia.research.java;
 import org.openjdk.jmh.annotations.*;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
